c2f3463d8a6b83919e5bce2652a861a602430093
lexnewgate <lexnewgate@gmail.com>
2020-08-09 23:40:14 +0800

    upgrade

---
+++ Assets/Test/Test.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+
+public class A
+{
+    public void Test()
+    {
+        var b = this as B;
+    }
+}
+
+public class B:A
+{
+
+}
